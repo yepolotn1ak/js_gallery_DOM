3cf5f5de6143cab0d467a7524c907406d029a18e
Yevhenii <polotnyak2004@gmail.com>
2024-06-06 15:08:18 +0300

    make build

---
+++ index.896be867.js
@@ -0,0 +1,2 @@
+document.querySelector(".gallery__list").addEventListener("click",function(e){e.preventDefault();var t=e.target.closest(".list-item__link");document.querySelector(".gallery__large-img").setAttribute("src",t.getAttribute("href"))});
+//# sourceMappingURL=index.896be867.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-shadow */ "use strict";
+const galary = document.querySelector(".gallery__list");
+galary.addEventListener("click", (event)=>{
+    event.preventDefault();
+    const smallLink = event.target.closest(".list-item__link");
+    const bigImage = document.querySelector(".gallery__large-img");
+    bigImage.setAttribute("src", smallLink.getAttribute("href"));
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
